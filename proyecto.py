9557b49ccbb4817665f627bfdfaa3ef7c4cfd84f
Matias Lara <129696532+Matias-Lara@users.noreply.github.com>
2024-10-06 14:50:05 -0300

    Restricciones, Compresion de dicc, Escribir .lp, (B_itd ESTA MAL)

--- proyecto.py
+++ proyecto.py
@@ -3,89 +3,120 @@ import numpy as np
 # GENERACION DE PARAMETROS
 I = range(2)  # Conjunto de asignaturas (DEPENDE DEL TAMANIO DE LA INSTANCIA)
 S = range(3)  # Conjunto de salas
-T = range(3)  # bloques horarios disponibles por dia
-D = range(1)  # Dias de la semana, de lunes a viernes
+T = range(7)  # Bloques horarios disponibles por dia
+D = range(5)  # Dias de la semana, de lunes a viernes
 
-# Capacidades
-C_s = {}
-for s in S:
-    capacidad = np.random.randint(20, 46)  # Capacidades aleatorias entre 20 y 45
-    C_s[s] = capacidad  # la sala s tendra capacidad "capacidad"
+C_s = {s: np.random.randint(20, 46) for s in S}  # Capacidad de salas aleatoria (20 a 45) (A=1)
+E_i = {i: np.random.randint(10, 41) for i in I}  # Estudiantes interesados (10 a 40) (A=1)
+A_i = {i: np.random.choice([0, 1], p=[0.8, 0.2]) for i in I}  # Asignatura indispensable (80% no, 20% si)
+P_i = {i: np.random.randint(6, 11) if A_i[i] == 1 else np.random.randint(1, 6) for i in I} #6-10 indispensable, 1-5 dispensable
+R_i = {i: np.random.choice([1, 2], p=[0.65, 0.35]) for i in I}  # i Requiere 1 o 2 bloques? (65% 1 bloque, 35% 2 bloques)(B=1)
 
-# Estudiantes interesados
-E_i = {}
-for i in I:
-    estudiantes = np.random.randint(10, 41)  # cant estudiantes aleatoria entre 10 y 40
-    E_i[i] = estudiantes  # la asignatura i tendra una cantidad "estudiantes"
+# Bloques bloqueados para profesores (LA DEFINIICION DE ESTE DICCIONARIO ESTA MALA, MODIFICAR)
+B_itd = {(i, t, d): np.random.choice([0, 1], p=[0.9, 0.1]) for i in I for t in T for d in D}
 
-# Asignaturas indispensables
-A_i = {}
-for i in I:
-    # 20% de prob de que la asignatura i sea indispensable
-    # 80%                   =                dispensable
-    A_i[i] = np.random.choice([0, 1], p=[0.8, 0.2])
-
-# Prioridades
-P_i = {}
-for i in I:
-    if A_i[i] == 1:
-        # Si la asignatura es indispensable, la prioridad se asigna entre 6 y 10
-        P_i[i] = np.random.randint(6, 11)
-    else:
-        # Si la asignatura no es indispensable, la prioridad se asigna entre 1 y 5
-        P_i[i] = np.random.randint(1, 6)
-
-
-# Bloques bloqueados por el horario del profesor
-B_itd = {}
-for i in I:
-    num_bloques_bloqueados = np.random.randint(7, 22) # un profesor tiene bloqueado entre 7 a 21 bloques semanalmente
-    
-    # Combinaciones t_d
-    combinaciones_t_d = []
-    for t in T:
-        for d in D:
-            combinaciones_t_d.append((t, d))
-    
-    # Se seleccionan aleatoriamente las combinaciones t_d bloqueadas que tendra el profesor de la asignatura i
-    bloques_bloqueados = np.random.choice(len(combinaciones_t_d), num_bloques_bloqueados, replace=False)
-    
-    # Se inicializan todos los bloques como disponibles
-    for t in T:
-        for d in D:
-            B_itd[(i, t, d)] = 0
-    
-    # Se asignan como bloqueados los escogidos aleatoriamente
-    for indice in bloques_bloqueados:
-        t, d = combinaciones_t_d[indice]
-        B_itd[(i, t, d)] = 1
-
-
-# La asignatura i necesita 1 o 2 bloques semanalmente?
-R_i = {}
-for i in I:
-    # Nuestro grupo es B = 1
-    # 65% de prob de que una asignatura tenga 1 bloque a la semana
-    # 35% de prob             =               2         =    
-    R_i[i] = np.random.choice([1, 2], p=[0.65, 0.35])
-
-# Variable de decision X_i_s_t_d
-variables = {}
-for i in I:
-    for s in S:
-        for t in T:
-            for d in D:
-                variables[(i, s, t, d)] = f'X_{i}_{s}_{t}_{d}'
+# Variables de decisión: si la asignatura i está asignada a la sala s, bloque t, día d
+variables = {(i, s, t, d): f'X_{i}_{s}_{t}_{d}' for i in I for s in S for t in T for d in D}
 
 # Funcion objetivo
 def generar_funcion_objetivo(variables, prioridades):
-    funcion_objetivo = ""
+    funcion_objetivo = "max: "
     for i in I:
         for s in S:
             for t in T:
                 for d in D:
-                    funcion_objetivo += f" + {prioridades[i]} * {variables[(i, s, t, d)]}"
-    return "Maximize\n" + funcion_objetivo
+                    funcion_objetivo += f"{prioridades[i]} * {variables[(i, s, t, d)]} + "
+    return funcion_objetivo.rstrip(" + ") + ";\n"
 
+# Restriccion 1: Evitar solapamientos en las salas
+def generar_restricciones_solapamientos(variables):
+    restricciones = ""
+    for s in S:
+        for t in T:
+            for d in D:
+                restriccion = " + ".join([variables[(i, s, t, d)] for i in I])
+                restricciones += f"{restriccion} <= 1;\n"
+    return restricciones
+
+# Restriccion 2: Asignacion de bloques requeridos por asignatura
+def generar_restricciones_asignacion_bloques_lp_solve(variables, requerimientos):
+    restricciones = ""
+    for i in I:
+        restriccion = ""
+        for s in S:
+            for t in T:
+                for d in D:
+                    restriccion += f"{variables[(i, s, t, d)]} + "
+        restricciones += f"{restriccion.rstrip(' + ')} = {requerimientos[i]};\n"
+    return restricciones
+
+# Restriccion 3: Capacidad de la sala
+def generar_restricciones_capacidad_lp_solve(variables, E_i, C_s):
+    restricciones = ""
+    for i in I:
+        for s in S:
+            for t in T:
+                for d in D:
+                    restricciones += f"{E_i[i]} * {variables[(i, s, t, d)]} <= {C_s[s]};\n"
+    return restricciones
+
+# Restriccion 4: Asignaturas indispensables deben asignarse
+def generar_restricciones_indispensables_lp_solve(variables, A_i):
+    restricciones = ""
+    for i in I:
+        if A_i[i] == 1:  # Si la asignatura es indispensable
+            restriccion = " + ".join([variables[(i, s, t, d)] for s in S for t in T for d in D])
+            restricciones += f"{restriccion} >= 1;\n"
+    return restricciones
+
+# Restriccion 5: Bloques bloqueados para los profesores
+def generar_restricciones_bloques_bloqueados_lp_solve(variables, B_itd):
+    restricciones = ""
+    for i in I:
+        for s in S:
+            for t in T:
+                for d in D:
+                    if B_itd[(i, t, d)] == 1:
+                        restricciones += f"{variables[(i, s, t, d)]} = 0;\n"
+    return restricciones
+
+# Restriccion 6: Bloques consecutivos
+def generar_restricciones_bloques_consecutivos_lp_solve(variables, R_i):
+    restricciones = ""
+    for i in I:
+        if R_i[i] == 2:  # Si la asignatura requiere 2 bloques consecutivos
+            for s in S:
+                for t in range(len(T) - 1):  # Hasta el penultimo bloque (t)
+                    for d in D:
+                        # Si se asigna al bloque t, también debe estar en el bloque t+1
+                        restricciones += f"{variables[(i, s, t, d)]} - {variables[(i, s, t+1, d)]} = 0;\n"
+    return restricciones
+
+# Declaracion de variables binarias
+def declarar_variables_binarias(variables):
+    binarios = "bin "
+    binarios += ", ".join(variables.values()) + ";\n"
+    return binarios
+
+# Generación de la función objetivo
 objetivo = generar_funcion_objetivo(variables, P_i)
-print(objetivo)
+
+# Generacion de restricciones
+restricciones_solapamientos = generar_restricciones_solapamientos(variables)
+restricciones_asignacion = generar_restricciones_asignacion_bloques_lp_solve(variables, R_i)
+restricciones_capacidad = generar_restricciones_capacidad_lp_solve(variables, E_i, C_s)
+restricciones_indispensables = generar_restricciones_indispensables_lp_solve(variables, A_i)
+restricciones_bloques_bloqueados = generar_restricciones_bloques_bloqueados_lp_solve(variables, B_itd)
+restricciones_bloques_consecutivos = generar_restricciones_bloques_consecutivos_lp_solve(variables, R_i)
+
+# Declaracion de las variables binarias
+declaracion_binarias = declarar_variables_binarias(variables)
+
+# Generacion del modelo completo en formato lp_solve
+modelo_lp_solve = objetivo + restricciones_solapamientos + restricciones_asignacion + restricciones_capacidad + restricciones_indispensables + restricciones_bloques_bloqueados + declaracion_binarias
+
+# Escribir el modelo en un archivo .lp
+with open('proyecto.lp', 'w') as file:
+    file.write(modelo_lp_solve)
+
+print("El archivo 'proyecto.lp' ha sido generado correctamente.")
